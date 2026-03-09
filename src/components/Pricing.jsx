04c9621750d7056cb8594ec920469abd1576c194
pratiksha chaudhary <pratikshachy80@gmail.com>
2026-03-09 22:24:49 +0545

    pricingtest

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,4 +1,4 @@
-import { CheckCircle2 } from "lucide-react"
+import { CheckCircle2, SpaceIcon } from "lucide-react"
 import { pricingOptions } from "../constants"
 
 const Pricing = () => {
@@ -7,7 +7,27 @@ const Pricing = () => {
       <h2 className="text-3xl sm:text-5xl lg:text-6xl my-8 tracking-wider">
         Pricing
       </h2>
-      <div className="flex flex-wrap"></div>
+      <div className="flex flex-wrap">
+        {pricingOptions.map((option, index) => (
+            <div key={index} className="w-full sm:w-1/2 lg:w-1/3 p-2">
+        <div className="p-10 border border-neutral-700 rounded-xl">
+            <p className="text-4xl mb-8">
+                {option.title}
+                {option.title == "Pro" && (
+                    <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text mb-4 ml-2">(Most Popular)</span>
+                    )}
+
+            </p>
+            <p className="mb-8">
+                <span className="">{option.price}</span>
+                <span>/Month</span>
+            </p>
+        </div>
+        </div>
+
+        ))}
+        
+      </div>
     </div>
   )
 }
